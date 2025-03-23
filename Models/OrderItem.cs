9df9e5a4e046aa253a468bde079defdca501d29d
Leandro Teixeira da Silva - LSI <leandro.silva@iteris.com.br>
2025-03-23 00:50:57 -0300

    Adicionado nome e forma de pagamento a tela de pedidos

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -1,14 +1,38 @@
-﻿namespace CantinaV1.Models
+﻿using SQLite;
+using System.ComponentModel;
+
+namespace CantinaV1.Models
 {
-    public class OrderItem
+    public class OrderItem : INotifyPropertyChanged
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string ClientName { get; set; }
         public string PaymentMethod { get; set; }
-        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal Price { get; set; }
-        public int Quantity { get; set; }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    OnPropertyChanged(nameof(Quantity));
+                    OnPropertyChanged(nameof(Total));
+                }
+            }
+        }
+
         public decimal Total => Price * Quantity;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Views/OrderPage.xaml.cs
+++ Views/OrderPage.xaml.cs
@@ -1,23 +1,74 @@
 using CantinaV1.Data;
 using CantinaV1.Models;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CantinaV1.Views;
 
-public partial class OrderPage : ContentPage
+public partial class OrderPage : ContentPage, INotifyPropertyChanged
 {
     private readonly Database _database;
-    private List<OrderItem> _orderItem;
     private List<Product> _product;
+    private string _clientName;
+    private string _paymentMethod;
+
+    public string ClientName
+    {
+        get => _clientName;
+        set
+        {
+            _clientName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsDinheiro
+    {
+        get => _paymentMethod == "Dinheiro";
+        set { if (value) PaymentMethod = "Dinheiro"; }
+    }
+
+    public bool IsPix
+    {
+        get => _paymentMethod == "Pix";
+        set { if (value) PaymentMethod = "Pix"; }
+    }
+
+    public bool IsPagarDepois
+    {
+        get => _paymentMethod == "Pagar depois";
+        set { if (value) PaymentMethod = "Pagar depois"; }
+    }
+
+    public bool IsCartao
+    {
+        get => _paymentMethod == "Cartão";
+        set { if (value) PaymentMethod = "Cartão"; }
+    }
+
+    public string PaymentMethod
+    {
+        get => _paymentMethod;
+        set
+        {
+            _paymentMethod = value;
+            OnPropertyChanged();
+        }
+    }
+
     public OrderPage()
     {
         InitializeComponent();
+        BindingContext = this;
+
         string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
             "cantina.db3");
         _database = new Database(dbPath);
 
         Inicializar();
     }
+
     private async void Inicializar()
     {
         await LoadOrderItemProducts();
@@ -37,11 +88,18 @@ public partial class OrderPage : ContentPage
         {
             OrderItem orderItem = new OrderItem()
             {
-                ProductName=product.Name,
-                Price = product.Price
+                ProductName = product.Name,
+                Price = product.Price,
+                Quantity = 0
             };
             orderItems.Add(orderItem);
         }
         return orderItems;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
