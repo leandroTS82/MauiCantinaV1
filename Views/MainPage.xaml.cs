6b4a5f089f61374f59648bfc59363f45a5723255
Leandro Teixeira da Silva - LSI <leandro.silva@iteris.com.br>
2025-04-03 00:00:27 -0300

    Botão de envio de msg teste para whatsapp

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CantinaV1.Views
+﻿using System.Diagnostics;
+
+namespace CantinaV1.Views
 {
     public partial class MainPage : ContentPage
     {
@@ -15,6 +17,21 @@
         private async void GoToOrderPage(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new OrderPage());
+        }
+        private void OnSendMessageClicked(object sender, EventArgs e)
+        {
+            string phoneNumber = "5511960842249";
+            string message = "Olá, esta é uma mensagem de teste!";
+            string url = $"https://wa.me/{phoneNumber}?text={Uri.EscapeDataString(message)}";
+
+            try
+            {
+                Launcher.OpenAsync(new Uri(url));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Erro ao abrir o WhatsApp: {ex.Message}");
+            }
         }
     }
 }
